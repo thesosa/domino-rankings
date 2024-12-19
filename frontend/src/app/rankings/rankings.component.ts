c037cde6e39ca9f7e937509753dab9c2041a1ad1
David Sosa <davidjsosav@gmail.com>
2024-12-19 17:13:54 +0200

    first version of rankings screen

--- frontend/src/app/rankings/rankings.component.ts
+++ frontend/src/app/rankings/rankings.component.ts
@@ -1,13 +1,110 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { model } from '../../../wailsjs/go/models';
+import { LoadMatches } from '../../../wailsjs/go/service/MatchService';
+import { LoadPlayers } from '../../../wailsjs/go/service/PlayerService';
+
+type PlayerRanking = {
+  player: model.Player;
+  totalMatches: number;
+  victories: number;
+  pointsEarned: number;
+  pointsLost: number;
+};
 
 @Component({
   selector: 'app-rankings',
   templateUrl: 'rankings.component.html',
 })
 export class RankingsComponent implements OnInit {
-  constructor() {}
+  matches: model.Match[] = [];
+  players: model.Player[] = [];
+  rankings: PlayerRanking[] = [];
 
-  ngOnInit(): void {}
+  constructor(private toastr: ToastrService) {}
+
+  async ngOnInit(): Promise<void> {
+    this.players = await LoadPlayers();
+    this.matches = await LoadMatches();
+    this.rankings = this.players.map((player) => {
+      const ranking = {
+        player,
+        totalMatches: 0,
+        victories: 0,
+        pointsEarned: 0,
+        pointsLost: 0,
+      };
+      return ranking;
+    });
+
+    // fill rankings data
+    this.matches.forEach((match) => {
+      // first, extract the relevant ranking objects
+      const playerA1Rank = this.rankings.find(
+        (ranking) => ranking.player.Name === match.TeamA.Player1.Name
+      );
+      const playerA2Rank = this.rankings.find(
+        (ranking) => ranking.player.Name === match.TeamA.Player2.Name
+      );
+      const playerB1Rank = this.rankings.find(
+        (ranking) => ranking.player.Name === match.TeamB.Player1.Name
+      );
+      const playerB2Rank = this.rankings.find(
+        (ranking) => ranking.player.Name === match.TeamB.Player2.Name
+      );
+
+      // figure out which team won
+      const teamAWon = match.TeamAPoints === 200;
+      // assign points and increased match count for each player
+      if (teamAWon) {
+        // team A
+        playerA1Rank!.totalMatches++;
+        playerA2Rank!.totalMatches++;
+        playerA1Rank!.victories++;
+        playerA2Rank!.victories++;
+        playerA1Rank!.pointsEarned += match.TeamAPoints;
+        playerA2Rank!.pointsEarned += match.TeamAPoints;
+        playerA1Rank!.pointsLost += match.TeamBPoints;
+        playerA2Rank!.pointsLost += match.TeamBPoints;
+
+        // team B
+        playerB1Rank!.totalMatches++;
+        playerB2Rank!.totalMatches++;
+        playerB1Rank!.pointsEarned += match.TeamBPoints;
+        playerB2Rank!.pointsEarned += match.TeamBPoints;
+        playerB1Rank!.pointsLost += match.TeamAPoints;
+        playerB2Rank!.pointsLost += match.TeamAPoints;
+      } else {
+        // team A
+        playerA1Rank!.totalMatches++;
+        playerA2Rank!.totalMatches++;
+        playerA1Rank!.pointsEarned += match.TeamAPoints;
+        playerA2Rank!.pointsEarned += match.TeamAPoints;
+        playerA1Rank!.pointsLost += match.TeamBPoints;
+        playerA2Rank!.pointsLost += match.TeamBPoints;
+
+        // team B
+        playerB1Rank!.totalMatches++;
+        playerB2Rank!.totalMatches++;
+        playerB1Rank!.victories++;
+        playerB2Rank!.victories++;
+        playerB1Rank!.pointsEarned += match.TeamBPoints;
+        playerB2Rank!.pointsEarned += match.TeamBPoints;
+        playerB1Rank!.pointsLost += match.TeamAPoints;
+        playerB2Rank!.pointsLost += match.TeamAPoints;
+      }
+    });
+
+    // sort rankings
+    this.rankings.sort((a, b) => {
+      const matchesDiff = b.totalMatches - a.totalMatches;
+      if (matchesDiff === 0) {
+        const victoriesDiff = b.victories - a.victories;
+        return victoriesDiff;
+      }
+      return matchesDiff;
+    });
+  }
 
   back(): void {
     window.history.back();
